e3652c79eff5ff10d4de59e09ef67dead0c6f9cc
LOYINuts <yushiko0510@gmail.com>
2025-02-17 16:31:08 +0800

    Embedding update format

--- model/Embedding.py
+++ model/Embedding.py
@@ -2,18 +2,28 @@ import torch.nn as nn
 import torch
 import math
 
+
 class KmerEmbedding(nn.Module):
-    def __init__(self,vocab_size,d_model):
-        super(KmerEmbedding,self).__init__()
-        self.encoder = nn.Embedding(vocab_size,d_model)
-    
-    def forward(self,x):
+    """Kmer,单词embedding"""
+
+    def __init__(self, vocab_size: int, d_model: int):
+        """初始化
+
+        Args:
+            vocab_size (int): 词表大小
+            d_model (int): 模型维度
+        """
+        super(KmerEmbedding, self).__init__()
+        self.encoder = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
         return self.encoder(x)
 
+
 class PositionEmbedding(nn.Module):
-    """位置编码
-    """
-    def __init__(self,max_len:int,d_model:int,device:str):
+    """位置编码"""
+
+    def __init__(self, max_len: int, d_model: int, device: str):
         """初始化
 
         Args:
@@ -22,34 +32,41 @@ class PositionEmbedding(nn.Module):
             device(str): str，训练设备
         """
         super(PositionEmbedding, self).__init__()
-        #初始化0矩阵
-        self.encoding = torch.zeros(max_len,d_model,device=device)
-        #位置编码不需要优化，就不需要梯度更新
+        # 初始化0矩阵
+        self.encoding = torch.zeros(max_len, d_model, device=device)
+        # 位置编码不需要优化，就不需要梯度更新
         self.encoding.requires_grad = False
-        #定义pos，生成位置索引
-        pos = torch.arange(0,max_len)
+        # 定义pos，生成位置索引
+        pos = torch.arange(0, max_len)
         pos = pos.to(device)
-        #类型转换为浮点型便于计算，在进行维度拓展为二维张量,利用广播机制自动对其
+        # 类型转换为浮点型便于计算，在进行维度拓展为二维张量,利用广播机制自动对其
         pos = pos.float().unsqueeze(dim=1)
-        #根据公式计算
+        # 根据公式计算
         frequencies_indices = torch.arange(0, d_model, step=2, device=device).float()
-        frequencies = 1.0/torch.pow(10000.0,frequencies_indices//d_model).unsqueeze(dim=0)
-        self.encoding[:,0::2] = torch.sin(pos*frequencies)
-        self.encoding[:,1::2] = torch.cos(pos*frequencies)
+        frequencies = 1.0 / torch.pow(
+            10000.0, frequencies_indices // d_model
+        ).unsqueeze(dim=0)
+        self.encoding[:, 0::2] = torch.sin(pos * frequencies)
+        self.encoding[:, 1::2] = torch.cos(pos * frequencies)
 
-    def forward(self,x):
-                #获取批量大小和序列长度
+    def forward(self, x):
+        # 获取批量大小和序列长度
         seq_len = x.size(1)
-        return self.encoding[:,:seq_len]
-    
+        return self.encoding[:, :seq_len]
+
 
 class FullEmbedding(nn.Module):
-    def __init__(self,vocab:int,d_model:int,max_len:int,drop_prob:float,device:str):
-        super(FullEmbedding,self).__init__()
-        self.token_emb = KmerEmbedding(vocab,d_model)
-        self.pos_emb = PositionEmbedding(max_len,d_model,device)
+    """embedding层"""
+
+    def __init__(
+        self, vocab: int, d_model: int, max_len: int, drop_prob: float, device: str
+    ):
+        super(FullEmbedding, self).__init__()
+        self.token_emb = KmerEmbedding(vocab, d_model)
+        self.pos_emb = PositionEmbedding(max_len, d_model, device)
         self.drop = nn.Dropout(drop_prob)
-    def forward(self,x):
+
+    def forward(self, x):
         tok_emb = self.token_emb(x)
         pos_emb = self.pos_emb(x)
-        return self.drop(tok_emb+pos_emb)
+        return self.drop(tok_emb + pos_emb)
